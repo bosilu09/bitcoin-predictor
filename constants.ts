d0911fb20ec8f3601fa8cac29fe5010ea68e5146
bosilu09 <84651759+bosilu09@users.noreply.github.com>
2025-05-30 21:05:25 +0530

    Delete constants.ts

--- constants.ts
+++
@@ -1,5 +0,0 @@
-
-export const COINGECKO_API_URL = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd';
-export const GEMINI_MODEL_NAME = 'gemini-2.5-flash-preview-04-17'; // General text tasks
-export const REFRESH_INTERVAL_MS = 60000; // 1 minute
-export const MAX_PRICE_HISTORY_LENGTH = 5; // Keep last 5 prices
